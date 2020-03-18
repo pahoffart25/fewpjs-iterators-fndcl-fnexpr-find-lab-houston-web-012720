66e8217babedf4c10c1014d1fe9d378668fa7847
pahoffart25 <pahoffart25@gmail.com>
2020-03-18 15:30:02 -0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// } why does this exist??????
+
+function superbowlWin(record){
+  const a = record.find(({result}) => result === "W") 
+  return !!a ? a.year : undefined
 }
